b29ab4d12d9cdd79de8b6965cf6adfd3009c1651
fuguhverdian <fahrulfuguhv@students.amikom.ac.id>
2023-01-05 23:39:37 +0700

    fahrul commit

--- NewRepoProject/NewRepoProject/Program.cs
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -8,11 +8,16 @@ namespace NewRepoProject
         {
             
             aldi();
+            fahrul();
         }
 
         static void aldi()
         {
             Console.WriteLine("21.11.4369");
+        }
+        static void fahrul()
+        {
+            Console.WriteLine("21.11.4331");
         }
     }
 
